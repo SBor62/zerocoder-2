82c0e46c34375d63db5348133ecbe2441c182272
SBor62 <velisavres@gmail.com>
2025-03-14 18:43:02 +0400

    Bank

---
+++ Main.py
@@ -0,0 +1,16 @@
+# Bank
+# Создаем функцию с циклом
+def bank(a, years):
+    for _ in range(years):
+        a += a * 0.10  # Увеличиваем вклад на 10%
+    return a
+
+
+# Ввод данных
+deposit = float(input("Введите сумму вклада: "))
+
+num_years = int(input("Введите срок вклада в годах: "))
+
+result = bank(deposit, num_years)
+
+print(f"Сумма на счету через {num_years} лет: {result} ")
